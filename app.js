176f19889a7245a4dd6889cdcfb79cdf7a73d02b
Jesper Qvarfordt <jesperqvarfordt@gmail.com>
2017-08-17 20:55:43 +0200

    project fully linted

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session')({
+  secret: 'any random string can go here',
+  resave: false,
+  saveUninitialized: false,
+});
+const User = require('./models/user');
 
 const index = require('./routes/index');
 const api = require('./routes/api/index');
@@ -15,23 +21,19 @@ const users = require('./routes/api/users');
 const app = express();
 
 // Connect to Mongoose
-mongoose.connect('mongodb://localhost/musiclist')
+mongoose.connect('mongodb://localhost/musiclist');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('express-session')({
-  secret: 'any random string can go here',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(expressSession);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -41,14 +43,13 @@ app.use('/api', api);
 app.use('/api/users', users);
 
 // Configure Passport
-const User = require('./models/user');
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
